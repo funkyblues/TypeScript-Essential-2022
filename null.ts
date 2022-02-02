456a690bf8d72d7c0b844c6a14376dc2c276168f
funkyblues <dnr8874@naver.com>
2022-02-02 13:07:56 +0900

    typescript null & undefined type

---
+++ null.ts
@@ -0,0 +1,8 @@
+// let MyName: string = null; // error 발생
+// let u: undefined = null; // error 발생
+
+let v: void = undefined; // OK
+
+let union: string | null = null;
+
+union = "wuk";
--- symbol.ts
+++
@@ -1,9 +0,0 @@
-console.log(Symbol("foo") === Symbol("foo"));
-
-const sym: symbol = Symbol(); // 아무것도 입력하지 않아도 생성됩니다.
-
-const obj = {
-  [sym]: "value",
-};
-
-obj[sym];
